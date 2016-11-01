810e6fea21a91364cea0e43d7c97c4e114111cdb
Artyom Desyatnikov <l0nikov@ya.ru>
2016-11-01 17:43:34 +0500

    +README

--- matrix_mul_test.py
+++ matrix_mul_test.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 import numpy
+import sys
 
 def print_m(m):
     print("%d %d"%m.shape)
     print("\n".join(" ".join(map(str,row)) for row in m))
-
-m1 = numpy.random.random((2000,300))
-m2 = numpy.random.random((300,400))
+n = int(sys.argv[1])
+m1 = numpy.random.random((n,n))
+m2 = numpy.random.random((n,n))
 print_m(m1)
 print("")
 print_m(m2)
--- matrix_mul_test.sh
+++ matrix_mul_test.sh
@@ -1,3 +1,3 @@
 export OMP_NUM_THREADS=$1
-s=${2:-1000000}
-make && ./matrix_mul_test.py | time ./bin/matrix_mul
+s=${2:-500}
+make && ./matrix_mul_test.py ${s} | time ./bin/matrix_mul
